90f9beb62714d87d2c03783c9672f8bc2ccd7a90
map72 <misty.periard@gmail.com>
2016-03-01 08:06:48 -0500

    Update Visit+CoreDataProperties.swift

--- Visit+CoreDataProperties.swift
+++ Visit+CoreDataProperties.swift
@@ -1,13 +1,3 @@
-//
-//  Visit+CoreDataProperties.swift
-//  CLVisit POC
-//
-//  Created by Misty Periard on 12/16/15.
-//  Copyright © 2015 Martyn Loughran. All rights reserved.
-//
-//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
-//  to delete and recreate this implementation file for your updated model.
-//
 
 import Foundation
 import CoreData
